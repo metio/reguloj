536b8e1a45a41f56c5f7d4fe9cb24042e0a0f6c1
Sebastian Hoß <mail@shoss.de>
2011-07-04 20:07:44 +0200

    Added missing testcase
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

---
+++ src/test/java/de/viadee/rules/implementation/CompositeConclusionTest.java
@@ -0,0 +1,74 @@
+package de.viadee.rules.implementation;
+
+import java.util.Collection;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+
+import com.google.common.collect.Lists;
+
+import de.viadee.rules.Conclusion;
+
+/**
+ * @author  Sebastian Hoß (sebastian.hoss@viadee.de)
+ * @since   2.0.0
+ */
+public class CompositeConclusionTest {
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    // *                                                  ATTRIBUTES                                                 *
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+    /** Checks expected exception inside single test cases. */
+    @org.junit.Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    // *                                                    TESTS                                                    *
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+    /**
+     * 
+     */
+    @SuppressWarnings("unused")
+    @Test
+    public void shouldNotAllowEmptyCollection() {
+        // given
+        final Collection<Conclusion<Object>> conclusions = Lists.newArrayList();
+
+        // when
+        this.thrown.expect(IllegalArgumentException.class);
+
+        // then
+        new CompositeConclusion<Object>(conclusions);
+    }
+
+    /**
+     * 
+     */
+    @SuppressWarnings("boxing")
+    @Test
+    public void shouldCallAllGivenConclusions() {
+        // given
+        final Object target = new Object();
+        final Collection<Conclusion<Object>> conclusions = Lists.newArrayList();
+        final Conclusion<Object> conclusion1 = Mockito.mock(Conclusion.class);
+        BDDMockito.given(conclusion1.apply(target)).willReturn(true);
+        final Conclusion<Object> conclusion2 = Mockito.mock(Conclusion.class);
+        BDDMockito.given(conclusion2.apply(target)).willReturn(false);
+
+        conclusions.add(conclusion1);
+        conclusions.add(conclusion2);
+
+        // when
+        final Conclusion<Object> composite = new CompositeConclusion<Object>(conclusions);
+
+        // then
+        Assert.assertTrue(composite.apply(target));
+    }
+
+}

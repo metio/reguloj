346b9143a1ebe57798d1737146409dd168ef321e
Sebastian Hoß <mail@shoss.de>
2013-09-28 00:20:23 +0200

    Update license header
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

--- src/main/java/com/github/sebhoss/reguloj/package-info.java
+++ src/main/java/com/github/sebhoss/reguloj/package-info.java
@@ -1,8 +1,8 @@
-/* This program is free software. It comes without any warranty, to
- * the extent permitted by applicable law. You can redistribute it
- * and/or modify it under the terms of the Do What The Fuck You Want
- * To Public License, Version 2, as published by Sam Hocevar. See
- * http://sam.zoy.org/wtfpl/COPYING for more details.
+/*
+ * Copyright © 2010 Sebastian Hoß <mail@shoss.de>
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
  */
 /**
  * This package holds all business rules related definitions.

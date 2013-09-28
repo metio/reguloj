5667370b29fc1792296d23188682bc286f14f23f
Sebastian Hoß <mail@shoss.de>
2013-09-28 21:47:05 +0200

    Added missing header
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

--- src/main/java/com/github/sebhoss/reguloj/AbstractRuleEngine.java
+++ src/main/java/com/github/sebhoss/reguloj/AbstractRuleEngine.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright © 2010 Sebastian Hoß <mail@shoss.de>
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
+ */
 package com.github.sebhoss.reguloj;
 
 import java.util.Set;

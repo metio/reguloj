be881e870c6b5b81c3a6a3a81e95932b91c02ed5
Sebastian Hoß <mail@shoss.de>
2013-09-28 21:18:15 +0200

    Use LimitedRuleEngine instead of UnchainedRuleEngine
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

--- src/main/java/com/github/sebhoss/reguloj/RuleEngines.java
+++ src/main/java/com/github/sebhoss/reguloj/RuleEngines.java
@@ -15,19 +15,19 @@ public final class RuleEngines {
     }
 
     /**
-     * @return A rule engine which does not support rule chaining.
-     */
-    public static <CONTEXT extends Context<?>> RuleEngine<CONTEXT> unchained() {
-        return new UnchainedRuleEngine<>();
-    }
-
-    /**
      * @param maximumNumberOfRuns
      *            The maximum number of runs to perform.
      * @return A rule engine which does not support rule chaining.
      */
     public static <CONTEXT extends Context<?>> RuleEngine<CONTEXT> limited(final int maximumNumberOfRuns) {
         return new LimitedRuleEngine<>(maximumNumberOfRuns);
+    }
+
+    /**
+     * @return A rule engine which evaluates all rules until the first rule that fires.
+     */
+    public static <CONTEXT extends Context<?>> RuleEngine<CONTEXT> firstWins() {
+        return new FirstWinsRuleEngine<>();
     }
 
     private RuleEngines() {

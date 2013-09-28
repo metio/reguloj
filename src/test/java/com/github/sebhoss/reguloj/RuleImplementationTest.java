81c5883d24b35a2a55c669b1062119c19dba9e01
Sebastian Hoß <mail@shoss.de>
2013-09-28 20:09:52 +0200

    Removed reference to hamcrest matchers
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

--- src/test/java/com/github/sebhoss/reguloj/RuleImplementationTest.java
+++ src/test/java/com/github/sebhoss/reguloj/RuleImplementationTest.java
@@ -7,17 +7,10 @@
 package com.github.sebhoss.reguloj;
 
 import com.github.sebhoss.common.annotation.CompilerWarnings;
-import com.github.sebhoss.reguloj.Conclusion;
-import com.github.sebhoss.reguloj.Context;
-import com.github.sebhoss.reguloj.Rule;
-import com.github.sebhoss.reguloj.RuleImplementation;
 import com.google.common.base.Predicate;
 
-import org.hamcrest.core.Is;
-import org.hamcrest.core.IsNull;
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 import org.mockito.BDDMockito;
 import org.mockito.Mockito;
 
@@ -28,23 +21,7 @@ import org.mockito.Mockito;
 public final class RuleImplementationTest {
 
     /** Constant name for all rules inside this test. */
-    private static final String NAME   = "test rule";             //$NON-NLS-1$
-
-    /** Checks expected exception inside single test cases. */
-    @org.junit.Rule
-    public ExpectedException    thrown = ExpectedException.none();
-
-    /**
-     * Ensures that <code>null</code> values are not permitted.
-     */
-    @Test
-    public void shouldNotAcceptNullValues() {
-        thrown.expect(NullPointerException.class);
-
-        final Rule<Context<Object>> rule = new RuleImplementation<>(null, null, null);
-
-        Assert.assertThat(rule, Is.is(IsNull.nullValue()));
-    }
+    private static final String NAME = "test rule"; //$NON-NLS-1$
 
     /**
      * Ensures that a valid rule will be built once all values are given.
@@ -56,7 +33,7 @@ public final class RuleImplementationTest {
 
         final Rule<Context<Object>> rule = new RuleImplementation<>(RuleImplementationTest.NAME, predicate, conclusion);
 
-        Assert.assertThat(rule, Is.is(IsNull.notNullValue()));
+        Assert.assertNotNull(rule);
     }
 
     /**
@@ -151,28 +128,26 @@ public final class RuleImplementationTest {
 
         final Rule<Context<Object>> rule = new RuleImplementation<>(RuleImplementationTest.NAME, predicate, conclusion);
 
-        Assert.assertThat(rule.getName(), Is.is(RuleImplementationTest.NAME));
+        Assert.assertEquals(rule.getName(), RuleImplementationTest.NAME);
     }
 
     /**
      * Ensures that equals is reflexive
      */
     @Test
-    @SuppressWarnings(CompilerWarnings.BOXING)
     public void equalsIsReflexive() {
         final Predicate<Context<Object>> predicate = Mockito.mock(Predicate.class);
         final Conclusion<Context<Object>> conclusion = Mockito.mock(Conclusion.class);
 
         final Rule<Context<Object>> rule = new RuleImplementation<>(RuleImplementationTest.NAME, predicate, conclusion);
 
-        Assert.assertThat(rule.equals(rule), Is.is(true));
+        Assert.assertEquals(rule, rule);
     }
 
     /**
      * Ensures that equals is symmetric
      */
     @Test
-    @SuppressWarnings(CompilerWarnings.BOXING)
     public void equalsIsSymmetric() {
         final Predicate<Context<Object>> predicate = Mockito.mock(Predicate.class);
         final Conclusion<Context<Object>> conclusion = Mockito.mock(Conclusion.class);
@@ -180,15 +155,14 @@ public final class RuleImplementationTest {
         final Rule<Context<Object>> rule1 = new RuleImplementation<>(RuleImplementationTest.NAME, predicate, conclusion);
         final Rule<Context<Object>> rule2 = new RuleImplementation<>(RuleImplementationTest.NAME, predicate, conclusion);
 
-        Assert.assertThat(rule1.equals(rule2), Is.is(true));
-        Assert.assertThat(rule2.equals(rule1), Is.is(true));
+        Assert.assertEquals(rule1, rule2);
+        Assert.assertEquals(rule2, rule1);
     }
 
     /**
      * Ensures that equals is transitive
      */
     @Test
-    @SuppressWarnings(CompilerWarnings.BOXING)
     public void equalsIsTransitive() {
         final Predicate<Context<Object>> predicate = Mockito.mock(Predicate.class);
         final Conclusion<Context<Object>> conclusion = Mockito.mock(Conclusion.class);
@@ -197,62 +171,41 @@ public final class RuleImplementationTest {
         final Rule<Context<Object>> rule2 = new RuleImplementation<>(RuleImplementationTest.NAME, predicate, conclusion);
         final Rule<Context<Object>> rule3 = new RuleImplementation<>(RuleImplementationTest.NAME, predicate, conclusion);
 
-        Assert.assertThat(rule1.equals(rule2) && rule2.equals(rule3), Is.is(true));
-        Assert.assertThat(rule3.equals(rule1), Is.is(true));
-    }
-
-    /**
-     * Ensures that equals is consistent
-     */
-    @Test
-    @SuppressWarnings(CompilerWarnings.BOXING)
-    public void equalsIsConsistent() {
-        final Predicate<Context<Object>> predicate = Mockito.mock(Predicate.class);
-        final Conclusion<Context<Object>> conclusion = Mockito.mock(Conclusion.class);
-
-        final Rule<Context<Object>> rule1 = new RuleImplementation<>(RuleImplementationTest.NAME, predicate, conclusion);
-        final Rule<Context<Object>> rule2 = new RuleImplementation<>(RuleImplementationTest.NAME, predicate, conclusion);
-
-        final boolean alwaysTheSame = rule1.equals(rule2);
-
-        for (int i = 0; i < 30; i++) {
-            Assert.assertThat(rule1.equals(rule2), Is.is(alwaysTheSame));
-        }
+        Assert.assertEquals(rule1, rule2);
+        Assert.assertEquals(rule2, rule3);
+        Assert.assertEquals(rule3, rule1);
     }
 
     /**
      * Ensures that equals is returns <code>false</code> on <code>null</code> values.
      */
     @Test
-    @SuppressWarnings(CompilerWarnings.BOXING)
     public void equalsReturnFalseOnNull() {
         final Predicate<Context<Object>> predicate = Mockito.mock(Predicate.class);
         final Conclusion<Context<Object>> conclusion = Mockito.mock(Conclusion.class);
 
         final Rule<Context<Object>> rule = new RuleImplementation<>(RuleImplementationTest.NAME, predicate, conclusion);
 
-        Assert.assertThat(rule.equals(null), Is.is(false));
+        Assert.assertNotEquals(rule, null);
     }
 
     /**
      * Ensures that equals is returns <code>false</code> on <code>null</code> values.
      */
     @Test
-    @SuppressWarnings(CompilerWarnings.BOXING)
     public void equalsReturnFalseOnWrongClass() {
         final Predicate<Context<Object>> predicate = Mockito.mock(Predicate.class);
         final Conclusion<Context<Object>> conclusion = Mockito.mock(Conclusion.class);
 
         final Rule<Context<Object>> rule = new RuleImplementation<>(RuleImplementationTest.NAME, predicate, conclusion);
 
-        Assert.assertThat(rule.equals(""), Is.is(false)); //$NON-NLS-1$
+        Assert.assertNotEquals(rule, ""); //$NON-NLS-1$
     }
 
     /**
      * Ensures that equals works with identical instances.
      */
     @Test
-    @SuppressWarnings(CompilerWarnings.BOXING)
     public void equalsWorks() {
         final Predicate<Context<Object>> predicate = Mockito.mock(Predicate.class);
         final Conclusion<Context<Object>> conclusion = Mockito.mock(Conclusion.class);
@@ -260,14 +213,13 @@ public final class RuleImplementationTest {
         final Rule<Context<Object>> rule1 = new RuleImplementation<>(RuleImplementationTest.NAME, predicate, conclusion);
         final Rule<Context<Object>> rule2 = rule1;
 
-        Assert.assertThat(rule1.equals(rule2), Is.is(true));
+        Assert.assertEquals(rule1, rule2);
     }
 
     /**
      * Ensures that equals works with different names
      */
     @Test
-    @SuppressWarnings(CompilerWarnings.BOXING)
     public void equalsWorksWithDifferentNames() {
         final Predicate<Context<Object>> predicate = Mockito.mock(Predicate.class);
         final Conclusion<Context<Object>> conclusion = Mockito.mock(Conclusion.class);
@@ -275,14 +227,13 @@ public final class RuleImplementationTest {
         final Rule<Context<Object>> rule1 = new RuleImplementation<>(RuleImplementationTest.NAME, predicate, conclusion);
         final Rule<Context<Object>> rule2 = new RuleImplementation<>("rule2", predicate, conclusion); //$NON-NLS-1$
 
-        Assert.assertThat(rule1.equals(rule2), Is.is(false));
+        Assert.assertNotEquals(rule1, rule2);
     }
 
     /**
      * Ensures that equals works with different predicates.
      */
     @Test
-    @SuppressWarnings(CompilerWarnings.BOXING)
     public void equalsWorksWithDifferentPremises() {
         final Predicate<Context<Object>> predicate1 = Mockito.mock(Predicate.class);
         final Predicate<Context<Object>> predicate2 = Mockito.mock(Predicate.class);
@@ -293,14 +244,13 @@ public final class RuleImplementationTest {
         final Rule<Context<Object>> rule2 = new RuleImplementation<>(RuleImplementationTest.NAME, predicate2,
                 conclusion);
 
-        Assert.assertThat(rule1.equals(rule2), Is.is(false));
+        Assert.assertNotEquals(rule1, rule2);
     }
 
     /**
      * Ensures that equals works with different conclusions.
      */
     @Test
-    @SuppressWarnings(CompilerWarnings.BOXING)
     public void equalsWorksWithDifferentConclusions() {
         final Predicate<Context<Object>> predicate = Mockito.mock(Predicate.class);
         final Conclusion<Context<Object>> conclusion1 = Mockito.mock(Conclusion.class);
@@ -311,32 +261,13 @@ public final class RuleImplementationTest {
         final Rule<Context<Object>> rule2 = new RuleImplementation<>(RuleImplementationTest.NAME, predicate,
                 conclusion2);
 
-        Assert.assertThat(rule1.equals(rule2), Is.is(false));
-    }
-
-    /**
-     * Ensures that hashCode() always returns the same value for a same instance.
-     */
-    @Test
-    @SuppressWarnings(CompilerWarnings.BOXING)
-    public void hashCodeIsSelfConsistent() {
-        final Predicate<Context<Object>> predicate = Mockito.mock(Predicate.class);
-        final Conclusion<Context<Object>> conclusion = Mockito.mock(Conclusion.class);
-
-        final Rule<Context<Object>> rule = new RuleImplementation<>(RuleImplementationTest.NAME, predicate, conclusion);
-
-        final int alwaysTheSame = rule.hashCode();
-
-        for (int i = 0; i < 30; i++) {
-            Assert.assertThat(rule.hashCode(), Is.is(alwaysTheSame));
-        }
+        Assert.assertNotEquals(rule1, rule2);
     }
 
     /**
      * Ensures that hashCode() is consistent with equals().
      */
     @Test
-    @SuppressWarnings(CompilerWarnings.BOXING)
     public void hashCodeIsConsistentWithEquals() {
         final Predicate<Context<Object>> predicate = Mockito.mock(Predicate.class);
         final Conclusion<Context<Object>> conclusion = Mockito.mock(Conclusion.class);
@@ -344,15 +275,14 @@ public final class RuleImplementationTest {
         final Rule<Context<Object>> rule1 = new RuleImplementation<>(RuleImplementationTest.NAME, predicate, conclusion);
         final Rule<Context<Object>> rule2 = new RuleImplementation<>(RuleImplementationTest.NAME, predicate, conclusion);
 
-        Assert.assertThat(rule1.equals(rule2), Is.is(true));
-        Assert.assertThat(rule1.hashCode(), Is.is(rule2.hashCode()));
+        Assert.assertEquals(rule1, rule2);
+        Assert.assertEquals(rule1.hashCode(), rule2.hashCode());
     }
 
     /**
      * Ensures that compareTo() is consistent with equals().
      */
     @Test
-    @SuppressWarnings(CompilerWarnings.BOXING)
     public void shoudCompareToOtherRules() {
         final Predicate<Context<Object>> predicate = Mockito.mock(Predicate.class);
         final Conclusion<Context<Object>> conclusion = Mockito.mock(Conclusion.class);
@@ -360,6 +290,6 @@ public final class RuleImplementationTest {
         final Rule<Context<Object>> rule1 = new RuleImplementation<>(RuleImplementationTest.NAME, predicate, conclusion);
         final Rule<Context<Object>> rule2 = new RuleImplementation<>(RuleImplementationTest.NAME, predicate, conclusion);
 
-        Assert.assertThat(rule1.compareTo(rule2), Is.is(0));
+        Assert.assertEquals(rule1.compareTo(rule2), 0);
     }
 }

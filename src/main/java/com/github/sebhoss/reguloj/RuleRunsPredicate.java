5cc14e381f91865060a6b61df8c73eb465b40ae8
Sebastian Hoß <mail@shoss.de>
2013-09-29 21:34:55 +0200

    Added missing license header
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

--- src/main/java/com/github/sebhoss/reguloj/RuleRunsPredicate.java
+++ src/main/java/com/github/sebhoss/reguloj/RuleRunsPredicate.java
@@ -1,4 +1,12 @@
+/*
+ * Copyright © 2010 Sebastian Hoß <mail@shoss.de>
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
+ */
 package com.github.sebhoss.reguloj;
+
+import javax.annotation.Nullable;
 
 final class RuleRunsPredicate<CONTEXT extends Context<?>> extends RulePredicate<CONTEXT> {
 
@@ -7,8 +15,8 @@ final class RuleRunsPredicate<CONTEXT extends Context<?>> extends RulePredicate<
     }
 
     @Override
-    public boolean apply(final Rule<CONTEXT> rule) {
-        return rule.run(context);
+    public boolean apply(final @Nullable Rule<CONTEXT> rule) {
+        return rule != null && rule.run(context);
     }
 
 }

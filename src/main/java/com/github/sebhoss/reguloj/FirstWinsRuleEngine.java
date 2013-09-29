4965f3b47f5f6c57347dcb4d6c9028a4773128da
Sebastian Hoß <mail@shoss.de>
2013-09-29 21:05:11 +0200

    Use FluentIterable
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

--- src/main/java/com/github/sebhoss/reguloj/FirstWinsRuleEngine.java
+++ src/main/java/com/github/sebhoss/reguloj/FirstWinsRuleEngine.java
@@ -8,15 +8,13 @@ package com.github.sebhoss.reguloj;
 
 import java.util.Set;
 
+import com.google.common.collect.FluentIterable;
+
 class FirstWinsRuleEngine<CONTEXT extends Context<?>> extends AbstractRuleEngine<CONTEXT> {
 
     @Override
     public void infer(final CONTEXT context, final Set<Rule<CONTEXT>> rules) {
-        for (final Rule<CONTEXT> rule : rules) {
-            if (rule.run(context)) {
-                break;
-            }
-        }
+        FluentIterable.from(rules).firstMatch(Rules.ruleRuns(context));
     }
 
 }

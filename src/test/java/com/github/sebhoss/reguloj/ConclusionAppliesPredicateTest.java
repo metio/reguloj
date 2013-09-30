3011744ce4eea15f9c383886048966ba6dd80160
Sebastian Hoß <mail@shoss.de>
2013-09-30 21:34:39 +0200

    Use @Before
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

--- src/test/java/com/github/sebhoss/reguloj/ConclusionAppliesPredicateTest.java
+++ src/test/java/com/github/sebhoss/reguloj/ConclusionAppliesPredicateTest.java
@@ -3,23 +3,34 @@ package com.github.sebhoss.reguloj;
 import com.github.sebhoss.common.annotation.CompilerWarnings;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 /**
  * Test cases for ConclusionAppliesPredicate.
  */
-@SuppressWarnings({ CompilerWarnings.BOXING, CompilerWarnings.NLS, CompilerWarnings.STATIC_METHOD,
+@SuppressWarnings({ CompilerWarnings.BOXING, CompilerWarnings.NLS, CompilerWarnings.NULL,
         CompilerWarnings.UNCHECKED })
 public class ConclusionAppliesPredicateTest {
+
+    private ConclusionPredicate<String> predicate;
+    private Conclusion<String>          conclusion;
+
+    /**
+     * Creates predicate and conclusion for each test.
+     */
+    @Before
+    public void setup() {
+        predicate = new ConclusionAppliesPredicate<>("test");
+        conclusion = Mockito.mock(Conclusion.class);
+    }
 
     /**
      * Ensures that a <code>true</code> is returned when the given conclusion applies.
      */
     @Test
     public void shouldReturnTrueOnAppliedConclusion() {
-        final ConclusionPredicate<String> predicate = new ConclusionAppliesPredicate<>("test");
-        final Conclusion<String> conclusion = Mockito.mock(Conclusion.class);
         Mockito.when(conclusion.apply("test")).thenReturn(Boolean.TRUE);
 
         final boolean applied = predicate.apply(conclusion);
@@ -32,8 +43,6 @@ public class ConclusionAppliesPredicateTest {
      */
     @Test
     public void shouldReturnFalseOnNonAppliedConclusion() {
-        final ConclusionPredicate<String> predicate = new ConclusionAppliesPredicate<>("test");
-        final Conclusion<String> conclusion = Mockito.mock(Conclusion.class);
         Mockito.when(conclusion.apply("test")).thenReturn(Boolean.FALSE);
 
         final boolean applied = predicate.apply(conclusion);

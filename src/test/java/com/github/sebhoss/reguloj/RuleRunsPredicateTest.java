1bc0883017fa896d368a9a817f1ab89255b72500
Sebastian Hoß <mail@shoss.de>
2013-09-30 22:58:27 +0200

    Added test case for RuleRunsPredicate
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

---
+++ src/test/java/com/github/sebhoss/reguloj/RuleRunsPredicateTest.java
@@ -0,0 +1,56 @@
+package com.github.sebhoss.reguloj;
+
+import com.github.sebhoss.common.annotation.CompilerWarnings;
+import com.google.common.base.Predicate;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+
+/**
+ * Test cases for RuleRunsPredicate.
+ */
+@SuppressWarnings({ CompilerWarnings.BOXING, CompilerWarnings.NULL, CompilerWarnings.UNCHECKED })
+public class RuleRunsPredicateTest {
+
+    private Context<Object>                  context;
+    private Rule<Context<Object>>            rule;
+    private Predicate<Rule<Context<Object>>> predicate;
+
+    /**
+     * Creates predicate and conclusion for each test.
+     */
+    @Before
+    public void setup() {
+        context = Mockito.mock(Context.class);
+        rule = Mockito.mock(Rule.class);
+        predicate = new RuleRunsPredicate<>(context);
+    }
+
+    /**
+     * Ensures that a <code>true</code> is returned when the given rule fires.
+     */
+    @Test
+    public void shouldReturnTrueOnFiringRule() {
+        BDDMockito.given(rule.run(context)).willReturn(Boolean.TRUE);
+
+        final boolean applied = predicate.apply(rule);
+
+        Assert.assertTrue(applied);
+    }
+
+    /**
+     * Ensures that a <code>false</code> is returned when the given rule does not fire.
+     */
+    @Test
+    public void shouldReturnFalseOnNonAppliedConclusion() {
+        BDDMockito.given(rule.run(context)).willReturn(Boolean.FALSE);
+
+        final boolean applied = predicate.apply(rule);
+
+        Assert.assertFalse(applied);
+    }
+
+}

bb1b339c0130c90405636d5b3b3651ec5ba1f7f7
Sebastian Hoß <mail@shoss.de>
2013-09-30 22:23:43 +0200

    Added test cases for FirstWinsRuleEngine
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

---
+++ src/test/java/com/github/sebhoss/reguloj/FirstWinsRuleEngineTest.java
@@ -0,0 +1,59 @@
+package com.github.sebhoss.reguloj;
+
+import com.github.sebhoss.common.annotation.CompilerWarnings;
+import com.google.common.collect.ImmutableList;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+/**
+ * Test case for FirstWinsRuleEngine.
+ */
+@SuppressWarnings({ CompilerWarnings.BOXING, CompilerWarnings.NULL, CompilerWarnings.UNCHECKED })
+public class FirstWinsRuleEngineTest {
+
+    private RuleEngine<Context<String>> engine;
+    private Context<String>             context;
+    private Rule<Context<String>>       rule1;
+    private Rule<Context<String>>       rule2;
+
+    /**
+     * Creates rule engine and context.
+     */
+    @Before
+    public void setup() {
+        engine = new FirstWinsRuleEngine<>();
+        context = Mockito.mock(Context.class);
+        rule1 = Mockito.mock(Rule.class);
+        rule2 = Mockito.mock(Rule.class);
+    }
+
+    /**
+     * Ensures that only the first matching rule will be run.
+     */
+    @Test
+    public void shouldOnlyRunFirstMatchingRule() {
+        Mockito.when(rule1.run(context)).thenReturn(Boolean.TRUE);
+
+        engine.infer(ImmutableList.of(rule1, rule2), context);
+
+        Mockito.verify(rule1, Mockito.times(1)).run(context);
+        Mockito.verifyZeroInteractions(rule2);
+    }
+
+    /**
+     * Ensures that the second rule will be run, if the first one did not.
+     */
+    @Test
+    public void shouldOnlyRunFirstMatchingRuleSecond() {
+        Mockito.when(rule1.run(context)).thenReturn(Boolean.FALSE);
+        Mockito.when(rule2.run(context)).thenReturn(Boolean.TRUE);
+
+        engine.infer(ImmutableList.of(rule1, rule2), context);
+
+        Mockito.verify(rule1, Mockito.times(1)).run(context);
+        Mockito.verify(rule2, Mockito.times(1)).run(context);
+    }
+
+}

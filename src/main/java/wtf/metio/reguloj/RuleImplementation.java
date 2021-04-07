397593d6b31add3f0ed6ac4353640264566b6ece
Sebastian Hoß <sebhoss@pm.me>
2021-04-07 05:28:01 +0200

    convert RuleImplementation to record

--- src/main/java/wtf/metio/reguloj/RuleImplementation.java
+++ src/main/java/wtf/metio/reguloj/RuleImplementation.java
@@ -1,20 +1,12 @@
 package wtf.metio.reguloj;
 
-import java.util.Objects;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
-final class RuleImplementation<CONTEXT extends Context<?>> implements Rule<CONTEXT> {
-
-  private final String name;
-  private final Predicate<CONTEXT> predicate;
-  private final Consumer<CONTEXT> consumer;
-
-  RuleImplementation(final String name, final Predicate<CONTEXT> predicate, final Consumer<CONTEXT> consumer) {
-    this.name = name;
-    this.predicate = predicate;
-    this.consumer = consumer;
-  }
+final record RuleImplementation<CONTEXT extends Context<?>>(
+    String name,
+    Predicate<CONTEXT> predicate,
+    Consumer<CONTEXT> consumer) implements Rule<CONTEXT> {
 
   @Override
   public void run(final CONTEXT context) {
@@ -26,24 +18,6 @@ final class RuleImplementation<CONTEXT extends Context<?>> implements Rule<CONTE
   @Override
   public boolean fires(final CONTEXT context) {
     return predicate.test(context);
-  }
-
-  @Override
-  public String name() {
-    return name;
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(name, predicate, consumer);
-  }
-
-  @Override
-  public boolean equals(final Object object) {
-    return object instanceof RuleImplementation<?> that
-        && Objects.equals(name, that.name)
-        && Objects.equals(predicate, that.predicate)
-        && Objects.equals(consumer, that.consumer);
   }
 
 }

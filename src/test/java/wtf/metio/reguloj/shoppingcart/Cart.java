e2f6656d7c124850eda4cbabeaf21891ce667c88
Sebastian Hoß <sebhoss@pm.me>
2021-04-07 23:25:10 +0200

    add license header

--- src/test/java/wtf/metio/reguloj/shoppingcart/Cart.java
+++ src/test/java/wtf/metio/reguloj/shoppingcart/Cart.java
@@ -1,3 +1,10 @@
+/*
+ * This file is part of reguloj. It is subject to the license terms in the LICENSE file found in the top-level
+ * directory of this distribution and at http://creativecommons.org/publicdomain/zero/1.0/. No part of reguloj,
+ * including this file, may be copied, modified, propagated, or distributed except according to the terms contained
+ * in the LICENSE file.
+ */
+
 package wtf.metio.reguloj.shoppingcart;
 
 import wtf.metio.reguloj.Context;

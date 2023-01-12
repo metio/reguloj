215e77a3d4e622c7c492f11a78b413a4472181ef
Sebastian Hoß <seb@xn--ho-hia.de>
2023-01-12 08:38:28 +0100

    add jspecify

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,10 @@
  * SPDX-FileCopyrightText: The reguloj Authors
  * SPDX-License-Identifier: 0BSD
  */
+@org.jspecify.annotations.NullMarked
 module wtf.metio.reguloj {
+
+    requires org.jspecify;
 
     exports wtf.metio.reguloj;
 
